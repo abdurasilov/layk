50594148f5de8f38cb23c69ee0a01c682f992be9
abdurasilov <65914532+abdurasilov@users.noreply.github.com>
2020-05-28 11:00:44 +0500

    Create uzb.php

---
+++ uzb.php
@@ -0,0 +1,18 @@
+<?php
+$btn1 = "💠 Post Yaratish";
+$btn2 = "⚙ Sozlamalar";
+$btn3 = "📩 Aloqa";
+$btn4 = "📋 Qo'llanma";
+$btn5 = "🚫 Bekor qilish";
+
+$txt1 = "🖐 Assalomu alaykum, hurmatli foydalanuvchi 👨‍💻 @UzxGroup Jamoasining $bot_user botiga hush kelibsiz\n Botdan unumli foydalanish uchun $btn4 bilan tanishib chiqing!\n\nSiz botimiz yordamida istalgan xabaringizga
+👍 Like, 🗯 Alert va 🔗 Url tugmachalarini biriktirib uni inline tizimida istalgan foydalanuvchiga yuborish imkoniyatiga ega boʻlasiz!\n\nBotdan unumli foydalanish uchun
+<a href='https://telegra.ph/Uz-Post-Bot-03-24'>📋 Qo'llanmani</a> o'qing";
+
+$azobol = "🖐<b>Assalomu alaykum, hurmatli foydalanuvchi 👨‍💻</b> @UzxGroup <b>jamoasini</b> $bot_user <b>botiga hush kelibsiz!\n\nSiz botimiz yordamida istalgan xabaringizga
+👍 Like, 🗯 Alert va 🔗 Url tugmachalarini biriktirib uni inline tizimida istalgan foydalanuvchiga yuborish imkoniyatiga ega boʻlasiz!\n‼️Botdan toʻliq foydalanish uchun</b> @Abdurasilov <b>kanaliga aʼzo boʻlishingiz kerak! Aʼzo boʻlganingizdan soʻng qayta</b> /start <b>bosing!</b>";
+
+$txt2 = "♻️ Istalgan turdagi xabarni menga yuboring xabar turi *🖼 Rasm, 🎬 Video, 🎲 Gif, 🎼 Musiqa, 🎤 Ovozli xabar* va boshqalardan tashkil topishi mumkun";
+$txt3 = "Botdan toʻgʻri va unumli foydalanish uchun ushbu qoʻllanmani oʻqib chiqing";
+$save = "♻️Yangi tugmalar saqlandi";
+?>
