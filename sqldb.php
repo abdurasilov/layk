b4eb9cca78ba593b6e84b04f67006b035145ba2a
abdurasilov <65914532+abdurasilov@users.noreply.github.com>
2020-05-28 11:05:10 +0500

    Create sqldb.php

---
+++ sqldb.php
@@ -0,0 +1,44 @@
+<?php 
+class Db{
+	protected $db;
+	protected $sql;
+
+
+	public function __construct($dbname){
+		$this->db = new PDO('sqlite:'.$dbname);
+		$this->sql = new SQLite3($dbname);
+	}
+
+
+	public function query($sql, $params = []) {
+		$stmt = $this->db->prepare($sql);
+		if (!empty($params)) {
+			foreach ($params as $key => $val) {
+				if (is_int($val)) {
+					$type = PDO::PARAM_INT;
+				} else {
+					$type = PDO::PARAM_STR;
+				}
+				$stmt->bindValue(':'.$key, $val, $type);
+			}
+		}
+		$stmt->execute();
+		return $stmt;
+	}
+
+	public function row($sql, $params = []) {
+		$result = $this->query($sql, $params);
+		return $result->fetchAll(PDO::FETCH_ASSOC);
+	}
+
+	public function onerow($sql, $params = []) {
+		$result = $this->query($sql, $params);
+		return $result->fetch(PDO::FETCH_ASSOC);
+	}
+
+	public function column($sql, $params = []) {
+		$result = $this->query($sql, $params);
+		return $result->fetchColumn();
+	}
+
+}
